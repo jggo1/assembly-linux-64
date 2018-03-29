389a042194040df93555af9f227a1e480d62a64f
jggo1 <jose.guilherme.oliveira.dev@gmail.com>
2018-03-29 15:11:47 -0300

    Update prog.asm

--- hello/prog.asm
+++ hello/prog.asm
@@ -2,22 +2,16 @@ section .data
 	msg	db  'Hello, world!',0xa
 	len	equ $ - msg
 
-	;console   db 1
-	;return	  db 0
-
-	;sys_write db 1
-	;sys_exite db 60
-
 section .text
 	global 	_start
 
 _start:
 	mov rdx, len
 	mov rsi, msg
-	mov rdi, 1;console
+	mov rdi, 1;stdin
 	mov rax, 1;sys_write
 	syscall
 	
 	mov rdi, 0;return
-	mov rax, 60;sys_exite
+	mov rax, 60;sys_exit
 	syscall
